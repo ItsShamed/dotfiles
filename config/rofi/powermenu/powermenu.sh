d4a30444b21079e1ff5125966908c37ec7d9d4e4
ItsShamed <thedjplays.gaming@gmail.com>
2021-10-27 00:07:59 +0200

    powermenu

--- config/rofi/powermenu/powermenu.sh
+++ config/rofi/powermenu/powermenu.sh
@@ -81,7 +81,7 @@ case $chosen in
         ;;
     $lock)
 		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
+			i3lock-fancy -- scrot -z -o
 		elif [[ -f /usr/bin/betterlockscreen ]]; then
 			betterlockscreen -l
 		fi
